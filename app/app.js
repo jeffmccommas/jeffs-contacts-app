db398eb58fc9376519f503c8f54bec9e609d3847
Beyonce Knowlesjeffmccommas <beyonce@thinkful.comjeffmccommas@gmail.com>
2015-08-14 06:09:12 -0400

    final code complete

--- app/app.js
+++ app/app.js
@@ -3,10 +3,10 @@
 // Declare app level module which depends on views, and components
 angular.module('myContacts', [
   'ngRoute',
+  'ngAnimate',
   'firebase',
+  'toastr',
   'myContacts.contacts'
-
-
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/contacts'});
